e1c1882187d1bafc5f506cc670c7e63c356065c3
eraybuyukkanat <erybykknt@gmail.com>
2024-03-11 16:35:53 +0300

    banner

--- src/layout/Banner/Banner.js
+++ src/layout/Banner/Banner.js
@@ -1,20 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import KeyboardArrowLeftRoundedIcon from "@mui/icons-material/KeyboardArrowLeftRounded";
 import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
-import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
 import { Box, Button } from "@mui/material";
-import { useTheme } from "@emotion/react";
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import { motion } from "framer-motion";
 
 const images = [
   {
-   
     imgPath:
       "https://wallpapers.com/images/featured-full/1920-x-1080-hd-1qq8r4pnn8cmcew4.jpg",
   },
   {
-   
     imgPath:
       "https://wallpapers.com/images/high/1920-x-1080-hd-2kph8n73fpmvddj1.webp",
   },
@@ -28,62 +23,82 @@ const images = [
   },
 ];
 function Banner() {
-    const theme = useTheme();
+  const maxSteps = images.length;
 
-    const [activeStep, setActiveStep] = useState(0);
-    const maxSteps = images.length;
-  
-    const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep === maxSteps-1 ? null : prevActiveStep + 1);
-    };
-  
-    const handleBack = (prev) => {
-     
-      setActiveStep((prevActiveStep) => prevActiveStep <= 0 ? null : prevActiveStep - 1);
-    };
-  
-    const handleStepChange = (step) => {
-      setActiveStep(step);
-    };
-  
+  const [index, setIndex] = useState(0);
+
+  const member1 = images[0 + index];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (index < maxSteps - 1) {
+        setIndex(index + 1);
+      } else {
+        setIndex(0);
+      }
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [index, maxSteps]);
+
+  function swipeToRight() {
+    if (index < maxSteps - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+  function swipeToLeft() {
+    if (index > 0) {
+      setIndex(index - 1);
+    } else {
+      setIndex(maxSteps - 1);
+    }
+  }
 
   return (
-    <Box sx={{ width: "100%", flexGrow: 1,alignItems:"center", justifyContent: "center", display: "flex"}}>
-   <Button onClick={handleBack} sx={{marginX: {xs: 0,md: 5},  color:"#008000"}}>
-      <KeyboardArrowLeftRoundedIcon sx={{paddingX: {xs:0,md: 5},width: 36,height: 36}}  />
-      </Button>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        springConfig={{
-          duration: "2s",
-          easeFunction: "ease-in-out",
-          
-          delay: "0s",
-        }}
-        loop={true}
-        enableMouseEvents
+    <Box
+      sx={{
+        width: "100%",
+        flexGrow: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+      }}
+    >
+      <Button
+        onClick={swipeToLeft}
+        sx={{ marginX: { xs: 0, md: 5 }, color: "#008000" }}
       >
-        {images.map((step, index) => (
-          <Box
-            key={step.label}
-            component="img"
-            sx={{
-              height: {xs: 200, md:600},
-              display: "block",
-              objectFit: "fill",
-              overflow: "hidden",
-              width: "100%",
-              borderRadius: 10
-            }}
-            src={step.imgPath}
-            alt={step.label}
-          />
-        ))}
-      </AutoPlaySwipeableViews>
-      <Button onClick={handleNext} sx={{marginX: {xs: 0,md: 5},  color:"#008000"}}>
-      <KeyboardArrowRightRoundedIcon sx={{paddingX: {xs:0,md: 5} ,width: 36,height: 36}}  />
+        <KeyboardArrowLeftRoundedIcon
+          sx={{ paddingX: { xs: 0, md: 5 }, width: 36, height: 36 }}
+        />
+      </Button>
+        <Box
+          key={member1.imgPath}
+          component={motion.img}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ delay: 0.2 }}
+          sx={{
+            height: { xs: 200, md: 600 },
+            display: "block",
+            objectFit: "fill",
+            overflow: "hidden",
+            width: "100%",
+            borderRadius: 10,
+          }}
+          src={member1.imgPath}
+          alt={member1.imgPath}
+        />
+      <Button
+        onClick={swipeToRight}
+        sx={{ marginX: { xs: 0, md: 5 }, color: "#008000" }}
+      >
+        <KeyboardArrowRightRoundedIcon
+          sx={{ paddingX: { xs: 0, md: 5 }, width: 36, height: 36 }}
+        />
       </Button>
     </Box>
   );
