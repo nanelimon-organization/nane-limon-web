64d5f2d32a4e60a2d3bc748c607165844396815f
eraybuyukkanat <erybykknt@gmail.com>
2024-06-22 22:19:15 +0300

    ++

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,42 +1,43 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../layout/Navbar/Navbar";
 import Footer from "../layout/Footer/Footer";
-import "./MainPage.css"
+import "./MainPage.css";
 import { useEffect, useState } from "react";
 import ChatBox from "../layout/Chatbot/ChatBox";
-function Main({scrollToSection}){
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }, []);
-    
+function Main({ scrollToSection }) {
+  const [sticky, setIsScrolled] = useState(false);
 
-      const [sticky,setSticky] = useState(false)
+  const handleScroll = () => {
+    const windowHeight = window.innerHeight;
+    const offset = window.scrollY;
+    if (offset > windowHeight * 0.9) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
 
-      useEffect(()=>{
-        window.addEventListener('scroll',()=>{
-          window.scrollY > 790 ? setSticky(true) : setSticky(false)
-        })
-      },[])
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return <>
-
+  return (
+    <>
       <div className="app-container">
+        <Navbar sticky={sticky} scrollToSection={scrollToSection} />
 
-        <Navbar sticky={sticky} scrollToSection={scrollToSection}  />
-        
         <main>
-       
-            <Outlet />
-
+          <Outlet />
         </main>
 
         <Footer />
-
-        </div>
-        <ChatBox />
-
+      </div>
+      <ChatBox />
     </>
+  );
 }
 
 export default Main;
-
