61e48e393445e509fb05ec5b8626291aeba6b64d
eraybuyukkanat <erybykknt@gmail.com>
2024-07-02 19:02:18 +0300

    ++

--- src/layout/Navbar/components/NavbarButton.js
+++ src/layout/Navbar/components/NavbarButton.js
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 import style from "./NavbarButton.module.css";
 import CustomButton from "../../../pages/HomePage/components/ContactForm/components/Button";
 import { motion } from "framer-motion";
-function NavbarButton({ title, to, onClick, color,fontSize}) {
+function NavbarButton({ title, to, onClick, color,fontSize,target}) {
   return (
     <Grid
       className={style.un}
       
     >
-      <NavLink to={to} >
-        <CustomButton onClick={onClick} title={title} color={color} fontSize={fontSize}/>
+      <NavLink to={to} target={target} >
+        <CustomButton onClick={onClick} title={title} color={color} fontSize={fontSize} />
       </NavLink>
     </Grid>
   );
--- src/layout/Navbar/components/NavbarItems.js
+++ src/layout/Navbar/components/NavbarItems.js
@@ -19,7 +19,7 @@ function NavbarItems({ scrollToSection }) {
 
   return (
     <>
-      <Grid display="flex" flexDirection="row">
+      <Grid display="flex" flexDirection="row" alignItems="center" justifyContent="center">
         <Grid
           component={motion.div}
           initial={{ opacity: 0 }}
@@ -83,6 +83,18 @@ function NavbarItems({ scrollToSection }) {
             title="Bize Ulaşın"
             to="/"
             onClick={()=>handleClick("contact")} 
+          />
+        </Grid>
+        <Grid
+          component={motion.div}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ delay: 0.7, type: "tween", stiffness: 100 }}
+        >
+          <NavbarButton
+            title="CooLLime"
+            to="https://cool-lime.vercel.app/" target="_blank"
           />
         </Grid>
       </Grid>
