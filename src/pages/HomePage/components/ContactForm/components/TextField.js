de35c678c2c0913277b5373741c3c2c744dfb7df
eraybuyukkanat <erybykknt@gmail.com>
2024-07-12 19:45:59 +0300

    +

--- src/pages/HomePage/components/ContactForm/components/TextField.js
+++ src/pages/HomePage/components/ContactForm/components/TextField.js
@@ -6,9 +6,10 @@ function CustomTextField({ label, multiline, rows, maxRows,type,error,value,name
   return (
     <Grid item xs={6} paddingX={2} paddingY={2}>
       <TextField
+        size="small"
         component={component}
         multiline={multiline}
-        rows={rows}
+        rows={rows} 
         InputProps={InputProps}
         error={error}
         type={type}
